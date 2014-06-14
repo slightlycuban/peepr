04d38f5b07da1851258ba5603c71a1463f186d94
Michael Tracy <slightlycuban@gmail.com>
2014-06-14 19:51:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peepr::Application.config.secret_token = '512052a465660984bb0e836045a11da978f2611a3b7818b44772eccf56fde4fa6d2288e701b1e0efd327714148e6166f7d6562ca669449d9837ecd90ba486688'
