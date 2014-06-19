6a834b3d92e5494ec01b4369fe1bdf2f1d6e1531
Michael Tracy <slightlycuban@gmail.com>
2014-06-19 08:49:40 -0400

    Create a Twitter client using oauth from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,13 @@ class User < ActiveRecord::Base
       user.oauth_secret = auth.credentials.secret
     end
   end
+
+  def client
+    @client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV["API_KEY"]
+      config.consumer_secret = ENV["API_SECRET"]
+      config.access_token = oauth_token
+      config.access_token_secret = oauth_secret
+    end
+  end
 end
