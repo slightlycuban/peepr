87f317004a701b26d965e36b8fefc129c20aaa80
Michael Tracy <slightlycuban@gmail.com>
2014-06-19 08:51:37 -0400

    Start building a tweet controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Peepr::Application.routes.draw do
 
+  get "tweet/all"
+
+  get "tweet/create"
+
+  get "tweet/view"
+  get "tweet/fetch"
+
   root :to => "static#home"
 
   match "/auth/twitter/callback" => "sessions#create"
