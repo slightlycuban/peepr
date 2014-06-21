0f0dcf703d423fe9baf0c82f02373b399ecb19a0
Michael Tracy <slightlycuban@gmail.com>
2014-06-21 17:27:58 -0400

    Change twitter_id to bigint since it is a big int

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140619231218) do
+ActiveRecord::Schema.define(:version => 20140621212034) do
 
   create_table "tweets", :force => true do |t|
     t.string   "content"
